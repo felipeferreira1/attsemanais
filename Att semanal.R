9816668c83e8e5e6abbe7c6893864c663c9f2145
Felipe Simplício Ferreira <felipe.ferreira@ipea.gov.br>
2019-11-15 14:59:52 -0300

    Multiplas atualizações
    Mudei o esquema de coleta de séries, usando as novas funções de coleta SGS e FRED sempre que necessário. Alterei a formatação dos dados que vem do sistema de expectativas do BC de modo a conseguir uma melhor organização dos dados colocando-os de acordo com a data de referência lado a lado. Não consegui usar funções nessa parte pois cada pedaço tem um padrão próprio. Também mudei os arquivos csv salvos aqui no git hub, devido a mudança na disposição dos dados.

--- Att semanal.R
+++ Att semanal.R
@@ -1,16 +1,68 @@
 #Rotina para coletar algumas séries do banco central para atualizações semanais
 #Feito por: Felipe Simplício Ferreira
-#última atualização: 21/10/2019
+#última atualização: 15/11/2019
 
 
 #Definindo diretórios a serem utilizados
 getwd()
-setwd("//srjn4/projetos/Projeto GAP-DIMAC/Automatizações/Att semanais")
+#setwd("//srjn4/projetos/Projeto GAP-DIMAC/Automatizações/Att semanais")
+setwd("C:/Users/User/Documents")
 
 #Carregando pacotes que serão utilizados
 library("zoo")
 library("fredr")
 library("dplyr")
+library("anytime")
+
+#Criando função para coleta de séries
+coleta_dados_sgs = function(series,datainicial="01/03/2011", datafinal = format(Sys.time(), "%d/%m/%Y")){
+  #Argumentos: vetor de séries, datainicial que pode ser manualmente alterada e datafinal que automaticamente usa a data de hoje
+  #Cria estrutura de repetição para percorrer vetor com códigos de séries e depois juntar todas em um único dataframe
+  for (i in 1:length(series)){
+    dados = read.csv(url(paste("http://api.bcb.gov.br/dados/serie/bcdata.sgs.",series[i],"/dados?formato=csv&dataInicial=",datainicial,"&dataFinal=",datafinal,sep="")),sep=";")
+    dados[,-1] = as.numeric(gsub(",",".",dados[,-1])) #As colunas do dataframe em objetos numéricos exceto a da data
+    nome_coluna = series[i] #Nomeia cada coluna do dataframe com o código da série
+    colnames(dados) = c('data', nome_coluna)
+    nome_arquivo = paste("dados", i, sep = "") #Nomeia os vários arquivos intermediários que são criados com cada série
+    assign(nome_arquivo, dados)
+    
+    if(i==1)
+      base = dados1 #Primeira repetição cria o dataframe
+    else
+      base = merge(base, dados, by = "data", all = T) #Demais repetições agregam colunas ao dataframe criado
+    print(paste(i, length(series), sep = '/')) #Printa o progresso da repetição
+  }
+  
+  base$data = as.Date(base$data, "%d/%m/%Y") #Transforma coluna de data no formato de data
+  base = base[order(base$data),] #Ordena o dataframe de acordo com a data
+  return(base)
+}
+
+#Criando função para coleta de séries do FRED
+coleta_dados_fred = function(series, datainicial="2018-01-01", datafinal = format(Sys.time(), "%Y-%m-%d")){
+  #Argumentos: vetor de séries, datainicial que pode ser manualmente alterada e datafinal que automaticamente usa a data de hoje
+  #Chave para funcionamento da API do FRED
+  fredr_set_key("759fd9f905b9d4025ce26e5ae6e63cb9")
+  #Cria estrutura de repetição para percorrer vetor com códigos de séries e depois juntar todas em um único dataframe
+  for (i in 1:length(series)){
+    dados = fredr(series[i], observation_start = as.Date(datainicial), observation_end = as.Date(datafinal))
+    nome_coluna = series[i] #Nomeia cada coluna do dataframe com o código da série
+    colnames(dados) = c('data', 'codigo da serie', nome_coluna)
+    nome_arquivo = paste("dados", i, sep = "") #Nomeia os vários arquivos intermediários que são criados com cada série
+    assign(nome_arquivo, dados)
+    
+    if(i==1)
+      base = dados1[,-2] #Primeira repetição cria o dataframe
+    else
+      base = merge(base, dados[,-2], by = "data", all = T) #Demais repetições agregam colunas ao dataframe criado
+    print(paste(i, length(series), sep = '/')) #Printa o progresso da repetição
+  }
+  
+  base$data = as.Date(base$data, "%d/%m/%Y") #Transforma coluna de data no formato de data
+  base = base[order(base$data),] #Ordena o dataframe de acordo com a data
+  base[,-1]=apply(base[,-1],2,function(x)as.numeric(gsub(",",".",x))) #Transforma o resto do dataframe em objetos numéricos
+  return(base)
+}
 
 #1) Meta para taxa over selic semanal
 meta_selic = read.csv(url(paste("https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativaMercadoMensais?$top=100&$skip=0&$filter=Indicador%20eq%20'Meta%20para%20taxa%20over-selic'&$orderby=Data%20desc&$format=text/csv&$select=Indicador,Data,DataReferencia,Mediana")))
@@ -19,30 +71,26 @@ meta_selic$DataReferencia = as.yearmon(meta_selic$DataReferencia, "%m/%Y")
 meta_selic = meta_selic[order(meta_selic$Data, meta_selic$DataReferencia),]
 meta_selic = meta_selic %>% filter(Data==tail(meta_selic$Data,1))
 meta_selic = subset(meta_selic, select = -c(Indicador))
+
 write.csv2(meta_selic,"01-meta_selic.csv", row.names = F)
 
-
-#Definindo data final (padrão usar a data da última sexta)
-datafinal = format(as.Date(meta_selic$Data[1]),"%d/%m/%Y")
-
-
 #2) Taxas de juros
-datainicial = "01/03/2011" #Padrão para todas as séries começarem juntas
-serie=c("432", "20717")
+serie1a = c("432")
+base1a = coleta_dados_sgs(serie1a, "01/03/2011")
 
-for (i in 1:length(serie)){
-  dados = read.csv(url(paste("http://api.bcb.gov.br/dados/serie/bcdata.sgs.",serie[i],"/dados?formato=csv&dataInicial=",datainicial,"&dataFinal=",datafinal,sep="")),sep=";")
-  dados$data = as.Date(dados$data, "%d/%m/%Y")
-  nome = paste("serie", i, sep = "")
-  assign(nome, dados)
-  if(i==1)
-    base1 = serie1
-  else
-    base1 = merge(base1, dados, by = "data", all = T)
-}
+# Coleta o valor da série em todos os últimos dias de cada mês
+ind = paste0(substr(base1a$data,1,4), "-",substr(base1a$data,6,7)) #paste0(ano,mes) mais eficiente que paste
+meta_mensal = tapply(base1a[,-1],ind, function(x) x[length(x)]) #Seleciona a coluna com os dados, usando o "ind" como índice e usa a função para pegar o último dado de acordo com o mês
+meta_mensal = as.data.frame.table(meta_mensal) #Transformando em dataframe
+names(meta_mensal)=c("data","meta selic")
+meta_mensal$data = anydate(meta_mensal$data)
 
-rm(dados)
-rm(list=objects(pattern="^serie"))
+serie1b = c("20717")
+base1b = coleta_dados_sgs(serie1b, "01/03/2011")
+
+base1 = merge(meta_mensal, base1b, by = "data", all.x = T)
+
+base1$data =  as.yearmon(base1$data, "%Y-%m-%d")
 
 names(base1)=c("Data","Meta Selic", "Taxa média de juros das operações de crédito com recursos livres - Total - % a.a.")
 
@@ -50,44 +98,13 @@ write.csv2(base1,"02-taxas_de_juros.csv", row.names = F)
 
 
 #3) Swaps diário, dólar, Ibovespa, SP&500
-datainicial = "22/05/2000" #Padrão para todas as séries começarem juntas
-serie=c("7806", "1", "7")
+serie2a = c("7806", "1", "7")
+base2a = coleta_dados_sgs(serie2a, "22/05/2000")
 
-for (i in 1:length(serie)){
-  dados = read.csv(url(paste("http://api.bcb.gov.br/dados/serie/bcdata.sgs.",serie[i],"/dados?formato=csv&dataInicial=",datainicial,"&dataFinal=",datafinal,sep="")),sep=";")
-  dados$data = as.Date(dados$data, "%d/%m/%Y")
-  nome = paste("serie", i, sep = "")
-  assign(nome, dados)
-  if(i==1)
-    base2.a = serie1
-  else
-    base2.a = merge(base2.a, dados, by = "data", all = T)
-}
+serie2b = c("SP500")
+base2b = coleta_dados_fred(serie2b, "2000-05-22")
 
-rm(dados)
-rm(list=objects(pattern="^serie"))
-
-fredr_set_key("759fd9f905b9d4025ce26e5ae6e63cb9")
-serie = c("SP500")
-
-for (i in 1:length(serie)){
-  dados = fredr(serie[i], observation_start = as.Date(datainicial, "%d/%m/%Y"), observation_end = as.Date(datafinal, "%d/%m/%Y"))
-  nome = paste("serie", i, sep = "")
-  assign(nome, dados)
-  if(i==1)
-    base2.b = serie1[,-2]
-  else
-    base2.b = merge(base2.b, dados[,-2], by = "date", all = T)
-}
-
-#Nomeando colunas do dataframe com todas as séries (para dar merge na data)
-names(base2.b) = c("data", serie)
-
-#Removendo resíduos
-rm(dados)
-rm(list=objects(pattern="^serie"))
-
-base2 = merge(base2.a, base2.b[-1,], by = "data", all = T)
+base2 = merge(base2a, base2b[-1,], by = "data", all = T)
 
 names(base2) = c("Data","7806 - Taxa referencial de swaps DI pré-fixada (BM&F) - Prazo de 360 dias - % a.a.",
                  "1 - Taxa de câmbio - Livre - Dólar americano (venda) - diário - u.m.c./US$",
@@ -99,23 +116,8 @@ write.csv2(base2,"03-Swaps_Dolar_Ibovespa_SP500.csv", row.names = F)
 #4) Dados tx juros real ex ante
 meta_selic_tabela = as.numeric(gsub(",", ".", base1$`Meta Selic`[length(base1$`Meta Selic`)]))
                                
-datainicial = "01/11/2001"
-serie=c("4390")
-
-for (i in 1:length(serie)){
-  dados = read.csv(url(paste("http://api.bcb.gov.br/dados/serie/bcdata.sgs.",serie[i],"/dados?formato=csv&dataInicial=",datainicial,"&dataFinal=",datafinal,sep="")),sep=";")
-  dados$data = as.Date(dados$data, "%d/%m/%Y")
-  nome = paste("serie", i, sep = "")
-  assign(nome, dados)
-  if(i==1)
-    base3 = serie1
-  else
-    base3 = merge(base3, dados, by = "data", all = T)
-}
-
-rm(dados)
-rm(list=objects(pattern="^serie"))
-
+serie3 = c("4390")
+base3 = coleta_dados_sgs(serie3, "01/11/2001")
 names(base3) = c("Data", "4390 - Selic acumulada no mês - % a.m.")
 selic_acum_mes = as.numeric(gsub(",", ".", base3$`4390 - Selic acumulada no mês - % a.m.`[length(base3$`4390 - Selic acumulada no mês - % a.m.`)]))
 
@@ -140,25 +142,9 @@ top5_expec_ipca_12meses_M =  top5_expec_ipca_12meses_M[1:12,]
 top5_expec_ipca_12meses_M$Mediana = ((as.numeric(gsub(",", ".",top5_expec_ipca_12meses_M$Mediana)))/100)+1
 media_top5_expec_ipca_12meses_M = (prod(top5_expec_ipca_12meses_M$Mediana) - 1)*100
 
-datainicial = "01/11/2001"
-serie=c("13522", "433")
-
-for (i in 1:length(serie)){
-  dados = read.csv(url(paste("http://api.bcb.gov.br/dados/serie/bcdata.sgs.",serie[i],"/dados?formato=csv&dataInicial=",datainicial,"&dataFinal=",datafinal,sep="")),sep=";")
-  dados$data = as.Date(dados$data, "%d/%m/%Y")
-  nome = paste("serie", i, sep = "")
-  assign(nome, dados)
-  if(i==1)
-    base4 = serie1
-  else
-    base4 = merge(base4, dados, by = "data", all = T)
-}
-
-rm(dados)
-rm(list=objects(pattern="^serie"))
-
+serie4=c("13522", "433")
+base4 = coleta_dados_sgs(serie4, "01/11/2001")
 names(base4) = c("Data", "13522 - Índice nacional de preços ao consumidor - amplo (IPCA) - em 12 meses", "433 - Índice nacional de preços ao consumidor-amplo (IPCA)")
-
 ipca_acum_12meses_mes_anterior = (as.numeric(gsub(",", ".",tail(base4$`13522 - Índice nacional de preços ao consumidor - amplo (IPCA) - em 12 meses`,1))))
 ipca_12_meses_mensal = (as.numeric(gsub(",", ".",tail(base4$`433 - Índice nacional de preços ao consumidor-amplo (IPCA)`,12)[1])))
 top5_expec_ipca_12meses_C = top5_expec_ipca_12meses %>% filter(tipoCalculo=="C") %>% filter(Data==tail(top5_expec_ipca_12meses$Data,1))
@@ -191,8 +177,18 @@ ipca$Data = as.Date(ipca$Data, "%Y-%m-%d")
 ipca = ipca[order(ipca$Data, decreasing = F),]
 ipca = ipca[order(ipca$DataReferencia),]
 ipca = subset(ipca, select = -c(baseCalculo, Indicador))
+for (i in 1:length(unique(ipca$DataReferencia))){
+  ano = (unique(ipca$DataReferencia))[i]
+  dados = ipca %>% filter(DataReferencia==ano)
+  dados = dados[-2]
+  colnames(dados) = c('Data', paste("Media", ano, sep = " "), paste("Mediana", ano, sep = " "))
+  if(i==1)
+    ipca_sep = dados
+  else
+    ipca_sep = left_join(ipca_sep, dados, by = "Data")
+}
 
-write.csv2(ipca,"05-Dados_IPCA.csv", row.names = F)
+write.csv2(ipca_sep,"05-Dados_IPCA.csv", row.names = F)
 
 
 #6) PIB trimestral Total
@@ -201,8 +197,19 @@ pib_trim$Data = as.Date(pib_trim$Data, "%Y-%m-%d")
 pib_trim$DataReferencia = as.yearqtr(pib_trim$DataReferencia, "%q/%Y")
 pib_trim = pib_trim[order(pib_trim$Data, pib_trim$DataReferencia),]
 pib_trim = subset(pib_trim, select = -c(Indicador))
+for (i in 1:length(unique(pib_trim$DataReferencia))){
+  ano = (unique(pib_trim$DataReferencia))[i]
+  dados = pib_trim %>% filter(DataReferencia==ano)
+  dados = dados[-2]
+  colnames(dados) = c('Data', paste("Media", ano, sep = " "), paste("Desvio Padrão", ano, sep = " "))
+  if(i==1)
+    pib_trim_sep = dados
+  else
+    pib_trim_sep = left_join(pib_trim_sep, dados, by = "Data")
+}
+pib_trim_sep = pib_trim_sep[,order(colnames(pib_trim_sep))]
 
-write.csv2(pib_trim,"06-PIB_trim.csv", row.names = F)
+write.csv2(pib_trim_sep,"06-PIB_trim.csv", row.names = F)
 
 
 #7) PIB anual Total
@@ -210,8 +217,19 @@ pib_anual = read.csv(url(paste("https://olinda.bcb.gov.br/olinda/servico/Expecta
 pib_anual$Data = as.Date(pib_anual$Data, "%Y-%m-%d")
 pib_anual = pib_anual[order(pib_anual$Data, pib_anual$DataReferencia),]
 pib_anual = subset(pib_anual, select = -c(Indicador))
+for (i in 1:length(unique(pib_anual$DataReferencia))){
+  ano = (unique(pib_anual$DataReferencia))[i]
+  dados = pib_anual %>% filter(DataReferencia==ano)
+  dados = dados[-2]
+  colnames(dados) = c('Data', paste("Media", ano, sep = " "), paste("Desvio Padrão", ano, sep = " "))
+  if(i==1)
+    pib_anual_sep = dados
+  else
+    pib_anual_sep = left_join(pib_anual_sep, dados, by = "Data")
+}
+pib_anual_sep = pib_anual_sep[,order(colnames(pib_anual_sep))]
 
-write.csv2(pib_anual,"07-PIB_anual.csv", row.names = F)
+write.csv2(pib_anual_sep,"07-PIB_anual.csv", row.names = F)
 
 
 #8) Resultado primário
@@ -220,8 +238,19 @@ result_prim$Data = as.Date(result_prim$Data, "%Y-%m-%d")
 result_prim = result_prim[order(result_prim$Data, result_prim$DataReferencia),]
 result_prim = result_prim %>% filter(IndicadorDetalhe=="Resultado PrimÃ¡rio")
 result_prim = subset(result_prim, select = -c(Indicador, IndicadorDetalhe))
+for (i in 1:length(unique(result_prim$DataReferencia))){
+  ano = (unique(result_prim$DataReferencia))[i]
+  dados = result_prim %>% filter(DataReferencia==ano)
+  dados = dados[-2]
+  colnames(dados) = c('Data', paste("Media", ano, sep = " "), paste("Desvio Padrão", ano, sep = " "))
+  if(i==1)
+    result_prim_sep = dados
+  else
+    result_prim_sep = left_join(result_prim_sep, dados, by = "Data")
+}
+result_prim_sep = result_prim_sep[,order(colnames(result_prim_sep))]
 
-write.csv2(result_prim,"08-result_prim.csv", row.names = F)
+write.csv2(result_prim_sep,"08-result_prim.csv", row.names = F)
 
 
 #9) Taxa cãmbio média do ano
@@ -230,8 +259,19 @@ tx_cambio_media$Data = as.Date(tx_cambio_media$Data, "%Y-%m-%d")
 tx_cambio_media = tx_cambio_media[order(tx_cambio_media$Data, tx_cambio_media$DataReferencia),]
 tx_cambio_media = tx_cambio_media %>% filter(IndicadorDetalhe=="MÃ©dia do ano")
 tx_cambio_media = subset(tx_cambio_media, select = -c(Indicador, IndicadorDetalhe))
+for (i in 1:length(unique(tx_cambio_media$DataReferencia))){
+  ano = (unique(tx_cambio_media$DataReferencia))[i]
+  dados = tx_cambio_media %>% filter(DataReferencia==ano)
+  dados = dados[-2]
+  colnames(dados) = c('Data', paste("Media", ano, sep = " "), paste("Desvio Padrão", ano, sep = " "))
+  if(i==1)
+    tx_cambio_media_sep = dados
+  else
+    tx_cambio_media_sep = left_join(tx_cambio_media_sep, dados, by = "Data")
+}
+tx_cambio_media_sep = tx_cambio_media_sep[,order(colnames(tx_cambio_media_sep))]
 
-write.csv2(tx_cambio_media,"09-tx_cambio_media.csv", row.names = F)
+write.csv2(tx_cambio_media_sep,"09-tx_cambio_media.csv", row.names = F)
 
 
 #10) Taxa cãmbio final do ano
@@ -240,5 +280,16 @@ tx_cambio_final$Data = as.Date(tx_cambio_final$Data, "%Y-%m-%d")
 tx_cambio_final = tx_cambio_final[order(tx_cambio_final$Data, tx_cambio_final$DataReferencia),]
 tx_cambio_final = tx_cambio_final %>% filter(IndicadorDetalhe=="Fim do ano")
 tx_cambio_final = subset(tx_cambio_final, select = -c(Indicador, IndicadorDetalhe))
+for (i in 1:length(unique(tx_cambio_final$DataReferencia))){
+  ano = (unique(tx_cambio_final$DataReferencia))[i]
+  dados = tx_cambio_final %>% filter(DataReferencia==ano)
+  dados = dados[-2]
+  colnames(dados) = c('Data', paste("Media", ano, sep = " "), paste("Desvio Padrão", ano, sep = " "))
+  if(i==1)
+    tx_cambio_final_sep = dados
+  else
+    tx_cambio_final_sep = left_join(tx_cambio_final_sep, dados, by = "Data")
+}
+tx_cambio_final_sep = tx_cambio_final_sep[,order(colnames(tx_cambio_final_sep))]
 
-write.csv2(tx_cambio_final,"10-tx_cambio_final.csv", row.names = F)
+write.csv2(tx_cambio_final_sep,"10-tx_cambio_final.csv", row.names = F)
